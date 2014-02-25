37f558e7892ae9034ae667b9551f88b13eb1a28e
tehaaron <aaronselvig@gmail.com>
2014-02-25 11:36:45 -0800

    added note to file for Matt
    added question for matt

--- src/Game.ts
+++ src/Game.ts
@@ -10,8 +10,13 @@ class Game {
 	constructor() {
 		this.player = new Player('tehaaron', new Deck(), new Inventory(), 100, 50, 0, 0 );
 
-		this.player.deck.addCard(new Card('TestGuy', 1, 1, 1, 10, 10, 0));
-
+		this.player.deck.addCard(new Card('TestGuy', 1, 1, 1, 10, 10, 0)); //This works!
+		/* These wont work:
+		this.player.deck.addCard(new Card('TestGuy', Ground, Human, Common, 10, 10, 0));
+		this.player.deck.addCard(new Card('TestGuy', Card.type.Ground, Card.race.Human, Card.rarity.Common, 10, 10, 0));
+		this.player.deck.addCard(new Card('TestGuy', Card.CardType.Ground, Card.CardRace.Human, Card.CardRarity.Common, 10, 10, 0));
+		plus a few more attempts. Why?
+		*/
 		console.log(this.player);
 	}
 } export = Game;
